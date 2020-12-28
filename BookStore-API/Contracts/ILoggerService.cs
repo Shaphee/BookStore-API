538c7b47f4520e6375bb0f0193638fc22d568e4a
Shafi Mohamed <shafi.laisprojects@gmail.com>
2020-12-28 13:42:26 +0530

    Created data classes and DTOs,  also included Auto mapper and changed appsetting file.

---
+++ BookStore-API/Contracts/ILoggerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Contracts
+{
+    //Here we have changed class to interface beacsue this is ILoggerService(I)
+    public interface ILoggerService
+    {
+        //create some some functions
+
+        void LogInfo(string message);
+        void LogWarn(string message);
+        void LogDebug(string message);
+        void LogError(string message);
+
+    }
+}
---
+++ BookStore-API/Controllers/Home1Controller.cs
@@ -0,0 +1,86 @@
+﻿using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore_API.Controllers
+{
+   
+    /// <summary>
+    /// This is test API controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Home1Controller : ControllerBase
+    {
+        private readonly ILoggerService _logger;
+
+        public Home1Controller(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+        /// <summary>
+        /// GET gets values
+        /// </summary>
+        /// <returns></returns>
+
+        // GET: api/<Home1Controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Accessed Home Controller");
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Get gets a value
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET api/<Home1Controller>/5
+        [HttpGet("{id}")]
+        
+        public string Get(int id)
+        {
+            _logger.LogDebug("Got a value");
+            return "value";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        // POST api/<Home1Controller>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _logger.LogError("This is an error");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        // PUT api/<Home1Controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        // DELETE api/<Home1Controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _logger.LogWarn("This is a warning");
+        }
+    }
+}
